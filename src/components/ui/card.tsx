c504b2ead2f4dfd10fd21bc6a476a48d138e1486
Nate Embrey <nate@ranadev.io>
2025-07-10 16:00:11 +0000

    the box seems to be flipping correctly in terms of bg color, etc. howeve

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -9,7 +9,7 @@ const Card = React.forwardRef<
   <div
     ref={ref}
     className={cn(
-      "rounded-lg border bg-card text-card-foreground shadow-sm transition-all duration-200 hover:scale-[1.01] hover:shadow-xl hover:shadow-foreground/5",
+      "rounded-lg border bg-card text-card-foreground shadow-sm",
       className
     )}
     {...props}
